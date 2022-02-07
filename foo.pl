474f8a18b81f44cd992d339103a4f1a6d7460918
Viktor Söderqvist <viktor.soderqvist@est.tech>
2022-02-07 17:36:05 +0100

    first commit

---
+++ foo.pl
@@ -0,0 +1,19 @@
+print <<'EOT';
+Test preview
+============
+
+Is this rendering properly?
+* bullet
+* bullet
+
+What about this?
+
+1. one
+1. two
+1. three
+
+That's it
+---------
+
+The end.
+EOT
